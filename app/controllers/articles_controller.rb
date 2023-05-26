89c72ec60f898a1d89f74fd24c7730186c4d7630
John Park <h.johnpark97@gmail.com>
2023-05-26 07:41:02 -0700

    create comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,43 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new # This is a new instance of the Article class
   end
+
+  def create
+    @article = Article.new(article_params) # This is a new instance of the Article class
+
+    if @article.save
+      redirect_to @article # This is a redirect to the show action
+    else
+      render 'new', status: :unprocessable_entity # This is a render of the new template
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id]) # This is a new instance of the Article class
+  end
+
+  def update
+    @article = Article.find(params[:id]) # This is a new instance of the Article class
+
+    if @article.update(article_params)
+      redirect_to @article # This is a redirect to the show action
+    else
+      render :edit, status: :unprocessable_entity # This is a render of the edit template
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to "/articles/1", status: :see_other # This is a redirect to the index action
+  end
+
+  private
+    def article_params
+      # submitted data from new.html.erb is available in params[:article]
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+    has_many :comments
+
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :body, presence: true, length: { minimum: 10, maximum: 300 }
 end
